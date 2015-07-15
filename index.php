150242b10d26d12ab7237e68db7ba17ec57944f6
Daniel Olsen <dani118c@stud.kea.dk>
2015-07-16 00:14:27 +0200

    Initial commit. First time with github instead of bitbucket!

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php include_once('dir.php'); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>I am not good With PC</title>
+
+		<meta charset="UTF-8">
+
+		<!-- Base URL -->
+
+		<!-- Styles -->
+		<link rel="stylesheet" href="stylesheets/scss/global.css">
+
+		<!-- Fonts -->
+
+		<!-- JQuery plugins -->
+		<script type="text/javascript" src="javascripts/jQuery/jquery-1.11.3.min.js"></script>
+
+		<!-- Custom Javascripts -->
+
+	</head>
+<body>
+
+	<header>
+
+		<p>Hello world!!</p>
+
+	</header>
+
+</body>
+</html>
---
+++ template/head.php
@@ -0,0 +1,14 @@
+<meta charset="UTF-8">
+
+<!-- Base URL -->
+<base href="">
+
+<!-- Styles -->
+<link rel="stylesheet" href="stylesheets/scss/global.css">
+
+<!-- Fonts -->
+
+<!-- JQuery plugins -->
+<script type="text/javascript" src="javascripts/jQuery/jquery-1.11.3.min.js"></script>
+
+<!-- Custom Javascripts -->
