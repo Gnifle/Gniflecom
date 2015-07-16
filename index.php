09388b74788365c3299050314b3fe5bfdb752b3e
Daniel Olsen <gnifle@gnifle.com>
2015-07-16 02:04:10 +0200

    Started work on header area.

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
 		<link rel="stylesheet" href="stylesheets/scss/global.css">
 
 		<!-- Fonts -->
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
 
 		<!-- JQuery plugins -->
 		<script type="text/javascript" src="javascripts/jQuery/jquery-1.11.3.min.js"></script>
@@ -22,11 +23,21 @@
 	</head>
 <body>
 
-	<header>
+	<div id="page">
+		<header>
 
-		<p>Hello world!</p>
+			<section>
+				<img src="images/layout/pix.png" class="logo">
+				<h1>Hi ! I'm <span class="white">Daniel</span>, a young designer from <span class="white">Denmark</span>. Welcome to my <span class="white">portfolio</span> !</h1>
+				<h1>Site is in <span class="white">progress</span>. Check back <span class="white">soon</span> !</h1>
+			</section>
 
-	</header>
+		</header>
+
+		<aside id="content">
+
+		</aside>
+	</div>
 
 </body>
 </html>
